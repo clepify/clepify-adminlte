022fdca16ac98a3b72aaf38daf562779cdf6a3d0
Andhika Dwi Khalisyahputra <andhikadwi980@gmail.com>
2024-02-28 18:15:31 +0700

    feat: add info card to dashboard

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,76 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<section class="py-3 py-md-5 py-xl-8 min-vh-100 d-flex align-items-center" style="background: linear-gradient(135deg, hsla(221, 57%, 33%, 1) 0%, hsla(229, 50%, 50%, 1) 100%);">
+    <div class="container">
+        <div class="row gx-2 align-items-center">
+        <div class="col-12 col-lg-7 d-none d-lg-block">
+            <div class="d-flex text-white">
+                <div class="col-10">
+                    <h1>{{ config('app.name', 'CLEPify') }}</h1>
+                    <hr class="border-primary-subtle mb-5">
+                    <h2 class="h1 mb-4">Campus Letter Efficiency Practicum</h2>
+                    <p class="lead mb-5">Sistem Informasi Manajemen Persuratan Mahasiswa Berbasis Web Di Jurusan Teknologi Informasi Politeknik Negeri Malang.</p>
+                    <div class="text-endx">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-grip-horizontal" viewBox="0 0 16 16">
+                        <path d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="col-12 col-lg-5">
+            <div class="card border-0 rounded-4">
+            <div class="card-body p-4 p-xl-5">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="mb-4">
+                            <h3><b>Reset Password</b></h3>
+                            @if(Session::has('account_deactivated'))
+                                <div class="alert alert-danger mt-3" role="alert">
+                                    {{ Session::get('account_deactivated') }}
+                                </div>
+                            @endif
+                            @error('email')
+                                <div class="alert alert-danger mt-3" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="row gy-3 overflow-hidden">
+                        <div class="col-12">
+                            <div class="form-floating mb-4">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" value="{{ old('email') }}" autocomplete="email" required autofocus>
+                                <label for="email" class="form-label">Email Address</label>
+                            </div>
+                            <div class="d-grid">
+                                <button class="btn btn-primary btn-lg" type="submit">Send Password Reset Link</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end mt-4">
+                            <a class="btn btn-link" href="{{ route('login') }}">
+                                Back to login
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
     </div>
-</div>
+</section>
 @endsection
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,13 +3,45 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1 class="ml-2">Dashboard</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-3">
+                <x-adminlte-button label="Kirim Surat" theme="primary" icon="fas fa-paper-plane" class="w-100 mb-4"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-3">
+                <x-adminlte-small-box title="424" text="Surat Aktif" icon="fas fa-envelope" theme="info" url="#" url-text="View details"/>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <x-adminlte-small-box title="424" text="Arsip Surat" icon="fas fa-box-archive" theme="info" url="#" url-text="View details"/>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <x-adminlte-small-box title="424" text="Mahasiswa" icon="fas fa-users" theme="info" url="#" url-text="View details"/>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <x-adminlte-small-box title="424" text="Views" icon="fas fa-check" theme="info" url="#" url-text="View details"/>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <style>
+        .small-box > .inner {
+            padding: 20px 10px 10px 20px;
+        }
+        .small-box h5 {
+            font-size: 1rem;
+            text-transform: uppercase;
+        }
+        .small-box > .small-box-footer {
+            text-align: start;
+            padding: 10px 20px;
+        }
+    </style>
 @stop
